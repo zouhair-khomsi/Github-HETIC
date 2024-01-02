6d0e49fdb8aea4403021012ff323a83ba219bf76
khadyndao02 <ndaok0244@gmail.com>
2024-01-02 20:28:53 +0100

    Hetic's project for improving Github skills

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'milligram/milligram',
+        'pretty_version' => '1.4.1',
+        'version' => '1.4.1.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'milligram/milligram' => array(
+            'pretty_version' => '1.4.1',
+            'version' => '1.4.1.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
